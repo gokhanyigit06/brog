df1d73b70589e8b9b212601d65ddf8735c994f5b
gokhanyigit06 <gokhanyigit1993@gmail.com>
2026-04-24 16:55:02 +0300

    fix: remove Firebase Auth (not enabled in console) - eliminates CONFIGURATION_NOT_FOUND error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,5 +15,4 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const auth = getAuth(app);
 export default app;
